a8cf232151de55c10118da9249c9471df3c95a84
Deepak <deepakalur4@gmail.com>
2024-04-12 17:57:16 +0530

    created virual env and setup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+def get_pack(file_path):
+    with open(file_path,"r") as file_obj:
+        pack=file_obj.readlines()
+        pack=[i.replace("\n","") for i in pack if i not in "-e ."]
+        return pack
+
+
+setup(
+    name="ml_project_practice",
+    version="0.0.0.0",
+    author="Deepak",
+    author_email="Deepakalur4@gmail.com",
+    packages=find_packages(),
+    install_requires=get_pack("requirements.txt")
+
+
+)
