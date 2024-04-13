82b7c63785d5789e0465184a94dbc31905234f0a
Deepak <deepakalur4@gmail.com>
2024-04-13 23:28:22 +0530

    Created the setup file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# Student-performance-predictor-ML-Project" 
 "# Student-performance-predictor-ML-Project" 
+"# Student-performance-predictor-ML-Project" 
--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_pack(file_path:str):
+def get_pack(file_path:str)-> list:
     '''
-    This function returns the list of packages in requierements file
+    This function returns the list of packages from requrirements file
     '''
-    with open(file_path,"r") as file_obj:
-        pack=file_obj.readlines()
-        pack=[i.replace("\n","") for i in pack if i not in "-e ."]
+    with open(file_path,"r") as pacakges:
+        pack=pacakges.readlines()
+        pack=[pac.replace("\n","") for pac in pack if pac not in "-e ."] 
         return pack
 
 
 setup(
-    name="ml_project_practice",
+    name="Student_performace_predictor_ML_Project",
     version="0.0.0.0",
-    author="Deepak",
+    author="Deepak S Alur",
     author_email="Deepakalur4@gmail.com",
     packages=find_packages(),
     install_requires=get_pack("requirements.txt")
+
+
 
 )
